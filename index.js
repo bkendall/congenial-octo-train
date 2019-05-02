1e5ed4a88e9da307ed48deb244283ba24f5feb31
Bryan Kendall <bkend@google.com>
2019-05-02 12:31:00 -0700

    move index to ts

--- index.js
+++
@@ -1,7 +0,0 @@
-const getName = require('./lib/getName');
-
-module.exports = () => {
-  const name = getName.getName();
-  console.log(`hello ${name}, here is my awesome module`);
-  return name;
-};
---
+++ index.ts
@@ -0,0 +1,7 @@
+import { getName } from './lib/getName';
+
+export default function hello(): string {
+  const name = getName();
+  console.log(`hello ${name}, here is my awesome module`);
+  return name;
+};
